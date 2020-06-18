e8c6f059dbe4c4088909e5ccfac77a89c5a6eafe
Luan Farias <matheus.frs22123@outlook.com>
2020-06-17 23:36:18 -0300

    :nail_care: Global styles was maden

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
+
 import HomePage from './pages/HomePage';
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
+            <GlobalStyles />
             <Switch>
                 <Route exact path="/" component={HomePage} />
             </Switch>
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        border: 0;
+        box-sizing: border-box;
+    }
+    body{
+        font-family: 'Poppins', sans-serif;
+        font-size: 16px;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        color: #95A5C6;
+        background-color: #161D26;
+    }
+    html, body, #root{
+        height: 100%;
+    }
+`;
